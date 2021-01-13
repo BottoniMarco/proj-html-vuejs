5b562a90b3098dda3210a2f67c6672c7d0764a1e
marco bottoni <bottoni.mrc@gmail.com>
2021-01-13 10:15:09 +0100

    footer 2.0

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    navbarLinks:[
+      "link1","link2","link3","link4","link5","link6","link7","link8"
+    ]
+  },
+  methods: {
+
+  },
+  created: function () {
+
+  }
+})
