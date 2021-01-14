6d67b925da99cff6abeaea24a344dac22500a7b1
marco bottoni <bottoni.mrc@gmail.com>
2021-01-14 19:05:44 +0100

    takeout 1.0

--- js/script.js
+++ js/script.js
@@ -2,14 +2,35 @@ var app = new Vue({
   el: "#app",
   data: {
     navbarLinks:[
-      "Home","Culinary History","Our Team","Our Menu","Takeout","Bulletin","reservations"
+        {link:"link", navitem:"Home", new:false},
+        {link:"link", navitem:"Culinary History", new:false}, {link:"link", navitem:"Our Menu", new:false},
+        {link:"link", navitem:"Takeout", new:true},
+        {link:"link", navitem:"Bulletin", new:false},
+        {link:"link", navitem:"reservations", new:false},        
     ],
-    footerCopyright:["Copyright 2012 - 2019 Avada |","All Rights Reserved |","Powered by WordPress |","ThemeFusion"]
+    footerCopyright:[
+      "All Rights Reserved |",
+      "Powered by WordPress |",
+      "ThemeFusion"
+    ],
+    footerSocial:[
+      {link:"facebook", social:"fab fa-facebook-f"},
+      {link:"twitter", social:"fab fa-twitter"},
+      {link:"youtube", social:"fab fa-youtube"},
+      {link:"instagram", social:"fab fa-instagram"},
+      {link:"linkedin", social:"fab fa-linkedin"}
+
+    ]
   },
   methods: {
 
   },
   created: function () {
+    var years = new Date().getFullYear();
+    var copyright = "Copyright 2012 -"+ years+"  Avada |"
+    console.log(copyright);
+    this.footerCopyright.unshift(copyright);
+    console.log(this.footerCopyright);
 
   }
 })
